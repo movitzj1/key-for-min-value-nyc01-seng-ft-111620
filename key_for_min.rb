6a07a229b89566f086e4ae3ec4ca0075bbd41577
Justin Movitz <movitzj1@montclair.edu>
2020-10-27 18:20:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,10 @@
 def key_for_min_value(name_hash)
   min_value = 1,000,000
   min_key = ""
-  name_hash.each do |key|
-    key.each do |value|
-      if value < min_value
-        min_value = value
-        min_key = key
-      end
+  name_hash.each do |key, value|
+    if value < min_value
+      min_value = value
+      min_key = key
     end
   end
   return min_key
