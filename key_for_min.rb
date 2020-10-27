9c34d93f07005ddb5507b07d0b1c1d4d22ff0541
Justin Movitz <movitzj1@montclair.edu>
2020-10-27 18:05:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  min_value = 100
+  name_hash.each do ||
+  end
+    if src[count][inner_count] < min
+        min = src[count][inner_count]
+      end
+      inner_count += 1
+    end
 
+  end
 end
