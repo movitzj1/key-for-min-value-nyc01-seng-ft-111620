b218e7be55ebbb1b26fa4befaeaf769be8cb94ca
Justin Movitz <movitzj1@montclair.edu>
2020-10-27 18:18:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
     key.each do |value|
       if value < min_value
         min_value = value
-        min_key = key
+        min_key << key
       end
     end
   end
